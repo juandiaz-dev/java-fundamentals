faaf84ee7267c43f483ebc63b0856bb2420e9ef9
Juan Diaz <106998637+juandiaz-dev@users.noreply.github.com>
2026-09-20 23:37:13 -0500

    feat: add string creation and comparison examples

---
+++ src/main/java/io/github/juandiazdev/fundamentals/conversions/strings/StringCreation.java
@@ -0,0 +1,22 @@
+package io.github.juandiazdev.fundamentals.conversions.strings;
+
+public class StringCreation {
+    public static void main(String[] args) {
+
+        String course = "Java Backend";
+        String course2 = new String("Java Backend");
+
+// Comparación primitiva usando '=='
+        boolean isSameReference = course == course2;
+        System.out.println("course == course2 : " + isSameReference);
+
+// Comparación orientada a objetos usando '.equals()'
+        boolean isSameValue = course.equals(course2);
+        System.out.println("course.equals(course2) : " + isSameValue);
+
+        String course3 = "Java Backend";
+// Como ambos son literales, Java recicla la memoria del String Pool
+        boolean isSamePoolReference = course == course3;
+        System.out.println("course == course3 : " + isSamePoolReference);
+    }
+}
